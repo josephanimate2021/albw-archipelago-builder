dde5d9bef2da6d7e026a1f9b25e138909953a03e
Joseph <100003036+josephanimate2021@users.noreply.github.com>
2025-11-03 17:17:42 -0500

    Update loadApp.js

--- loadApp.js
+++ loadApp.js
@@ -1,18 +1,34 @@
 const cmd = require("child_process");
 const fs = require("fs");
-const builder = require("./backend/builder");
 
-if (!fs.existsSync('node_modules') || !fs.existsSync('package-lock.json')) {
-    console.log("All required app dependecies do not exist! Installing them...");
-    builder.shellOutput(cmd.spawn('npm', ['install'])).then(() => {
-        console.log("Dependecies were successfuly installed! Starting app...");
-        startApp();
+if (fs.existsSync('.git')) {
+    console.log('Checking for updates...');
+    runCommand('start git pull').then(() => {
+        console.log("All updates were checked successfuly!");
+        runChecks();
     });
-} else {
-    console.log("All app dependecies are currently installed. Starting the app...");
-    startApp();
-}
+} else runChecks();
+
+async function runChecks() {
+    if (!fs.existsSync('node_modules') || !fs.existsSync('package-lock.json')) {
+        console.log("All required app dependecies do not exist! Installing them...");
+        await runCommand("npm install");
+        console.log("All app dependecies were installed successfuly! Starting the app...");
+        startApp();
+    } else {
+        console.log("All app dependecies are currently installed. Starting the app...");
+        startApp();
+    }
 
 function startApp() {
-    cmd.execSync("start npm start");
+    runCommand("start npm start");
+}
+
+function runCommand(command) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            cmd.execSync(command);
+            res();
+        }, 4167);
+    });
 }
