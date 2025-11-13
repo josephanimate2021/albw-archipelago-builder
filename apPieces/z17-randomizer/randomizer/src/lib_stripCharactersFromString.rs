9caa254452d08272e7d28eeb427b92be267fa4ec
Joseph <100003036+josephanimate2021@users.noreply.github.com>
2025-11-13 15:59:35 -0500

    Refactor ArchipelagoItem and ArchipelagoInfo structures

--- apPieces/z17-randomizer/randomizer/src/lib_stripCharactersFromString.rs
+++ apPieces/z17-randomizer/randomizer/src/lib_stripCharactersFromString.rs
@@ -5,6 +5,29 @@ fn sanitize(string: &str) -> String {
     remove.replace_all(&to_space.replace_all(string, " "), "").to_string()
 }
 
+#[derive(Serialize, Debug, Clone)]
+#[pyclass]
+pub struct ArchipelagoItem {
+    pub name: String,
+    pub classification: u8,
+}
+
+#[pymethods]
+impl ArchipelagoItem {
+    pub const CLASS_PROGRESSION: u8 = 1;
+    pub const CLASS_USEFUL: u8 = 2;
+    pub const CLASS_TRAP: u8 = 4;
+
+    #[new]
+    pub fn new(name: String, classification: u8) -> ArchipelagoItem {
+        ArchipelagoItem {name, classification}
+    }
+
+    pub fn is_major(&self) -> bool {
+        self.classification & Self::CLASS_PROGRESSION != 0
+    }
+}
+
 #[derive(Serialize, Default, Debug, Clone)]
 #[pyclass]
 pub struct ArchipelagoInfo {
@@ -12,7 +35,7 @@ pub struct ArchipelagoInfo {
     pub name: String,
 
     #[pyo3(get, set)]
-    pub item_names: DashMap<String, String>,
+    pub items: DashMap<String, ArchipelagoItem>,
 }
 
 #[pymethods]
@@ -25,9 +48,9 @@ impl ArchipelagoInfo {
 
 impl ArchipelagoInfo {
     pub fn get_item_name(&self, location_name: &str) -> Result<String> {
-        self.item_names
+        self.items
             .get(location_name)
-            .map(|s| sanitize(s))
+            .map(|item| sanitize(&item.name))
             .ok_or(Error::internal(format!("Patch file does not contain an item for location {}", location_name)))
     }
 }
