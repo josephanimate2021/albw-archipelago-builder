ca7e678d0b3069125a1c0808122869df0bb501d2
Joseph <100003036+josephanimate2021@users.noreply.github.com>
2025-11-23 10:54:46 -0500

    Improve formatting of Python path exports in bashrc
    Added newline characters before appending Python path exports to .bashrc for better readability.

--- utilities/python_linux.sh
+++ utilities/python_linux.sh
@@ -5,9 +5,9 @@ echo Setting Python Paths...
 PYENV_ROOT_EXPORT='export PYENV_ROOT="$HOME/.pyenv"'
 PYENV_PATH='[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"'
 PYENV_INIT_EXEC='eval "$(pyenv init - bash)"'
-echo "$PYENV_ROOT_EXPORT" >> ~/.bashrc
-echo "$PYENV_PATH" >> ~/.bashrc
-echo "$PYENV_INIT_EXEC" >> ~/.bashrc
+echo "\n$PYENV_ROOT_EXPORT" >> ~/.bashrc
+echo "\n$PYENV_PATH" >> ~/.bashrc
+echo "\n$PYENV_INIT_EXEC" >> ~/.bashrc
 echo Python paths were successfully set!
 pyenv install 3.12.0
 pyenv global 3.12.0
