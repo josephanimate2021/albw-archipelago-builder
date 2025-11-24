7a46218362697bcb67590033c8016410e4e3c88d
Joseph <100003036+josephanimate2021@users.noreply.github.com>
2025-11-24 00:41:28 -0500

    Update python_linux.sh

--- utilities/python_linux.sh
+++ utilities/python_linux.sh
@@ -1,14 +1,6 @@
 echo Installing pyenv...
-curl -fsSL https://pyenv.run | bash
-echo Successfuly installed pyenv!
-echo Setting Python Paths...
-PYENV_ROOT_EXPORT='export PYENV_ROOT="$HOME/.pyenv"'
-PYENV_PATH='[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"'
-PYENV_INIT_EXEC='eval "$(pyenv init - bash)"'
-echo "$PYENV_ROOT_EXPORT" >> ~/.bashrc
-echo "$PYENV_PATH" >> ~/.bashrc
-echo "$PYENV_INIT_EXEC" >> ~/.bashrc
-echo Python paths were successfully set!
+curl https://pyenv.run | bash
+echo Successfuly installed pyenv! Installing Python version 3.12.11...
 pyenv install 3.12.11
 pyenv global 3.12.11
 echo Python has been downloaded and installed. The CLI Installer will be exiting in a few seconds.
