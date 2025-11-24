b3619d009394f757f2420eaac6af5b32e7ceac3c
Joseph <100003036+josephanimate2021@users.noreply.github.com>
2025-11-24 00:56:37 -0500

    Modify python_linux.sh for pyenv setup
    Updated installation script to set up Python environment variables in .bashrc.

--- utilities/python_linux.sh
+++ utilities/python_linux.sh
@@ -1,6 +1,9 @@
 echo Installing pyenv...
 curl https://pyenv.run | bash
-echo Successfuly installed pyenv! Installing Python version 3.12.11...
+echo Successfuly installed pyenv! Setting up Python path...
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 pyenv install 3.12.11
 pyenv global 3.12.11
 echo Python has been downloaded and installed. The CLI Installer will be exiting in a few seconds.
